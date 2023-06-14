c6fa42df33cd5ba221d4eb18c5cfd3da342768fc
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-06-14 23:01:09 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // code your solution here
 function saturdayFun(activity = "roller-skate"){
-    returns `This Saturday, I want to ${activity}!`;
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
 }
