c3d3a5fa0e4504607d6cbdfc947493e49365bd79
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-06-21 13:00:55 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ function mondayWork(activity = 'go to the office'){
     return `This Monday, I will ${activity}.`
 }
 
-function wrapAdjective(){
-    
+function wrapAdjective(symbol){
+    return function(adjective){
+        return `You are ${symbol}${adjective}${symbol}!`
+    }
 }
